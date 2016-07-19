9e22f6cf7ef96a386267f5d16f85137d8057cec2
Vinicius Cainelli <vicainelli@gmail.com>
2016-07-19 17:52:21 -0300

    Aula 3

--- angularjs-mvc/alurapic/public/js/controllers/fotos-controller.js
+++ angularjs-mvc/alurapic/public/js/controllers/fotos-controller.js
@@ -1,6 +1,13 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
-	$scope.foto = {
-		titulo: 'Leão',
-		url: 'https://i.ytimg.com/vi/WpHpadm0cB0/hqdefault.jpg'
-	};
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+
+	$scope.fotos = [];
+
+	$http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	})
+	.error(function(erro){
+		console.log(erro);
+	});
+
 });
